93864dc5085f7f7b9190347c5b8869415080416f
sababakhtiar <sababakhtiar23@gmail.com>
2024-05-10 09:38:30 +0500

    add 2nd commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <Navbar/>
   
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" exact element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/service" element={<Service/>}/>
         <Route path="/contact" element={<Contact/>}/>
